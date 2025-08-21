1669b00d6e4357d7acafeafc9e8910c715a41415
David <p5y4@laposte.net>
2025-08-21 18:39:18 +0200

    second commit

--- src/app/__component/form/UploadForm.tsx
+++ src/app/__component/form/UploadForm.tsx
@@ -36,7 +36,7 @@ export const UploadForm = () => {
             }
         },
     })
-
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     let heic2any: any;
 
     const handleFileChange = async (
